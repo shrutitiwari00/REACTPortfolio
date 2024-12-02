d2a41e6648ca89a1a6664ac5b0769ecbc47c360f
unknown <you@example.com>
2024-12-02 13:38:08 +0530

    Added my projct

---
+++ src/data/ProjectsList.jsx
@@ -0,0 +1,59 @@
+export const ProjectsList = {
+  projects: [
+    {
+      name: "Blog Website",
+      description:
+        "It's a blog website created by using React.js. Using the concept of Dynamic Routing and Context API'S. It is styled using CSS3.",
+      technologies: [
+        "React JS",
+        "Context APIs",        
+        "CSS",
+        "Git",
+        "GitHub",
+      ],
+    },
+    {
+      name: "Weather App",
+      description:     
+        "A responsive web application that provides real-time weather updates. Built HTML, CSS and React.js,the app fetches data from a weather API to display current conditions and forecasts for various locations.",
+      technologies: [
+        "React JS",
+        "REST APIs",
+        "CSS",
+        "Git",
+        "GitHub",
+        
+      ],
+    },
+    {
+      name: "Personal Portfolio",
+      description:
+        "My portfolio is a website built with React.js and Tailwind CSS, utilizing React Router v6 to enable easy access to multiple pages. It was designed and created entirely by myself, showcasing my skills and projects. The website is responsive and visually appealing, providing a user-friendly experience.",
+      technologies: ["React JS", "Tailwind CSS", "Git", "GitHub", "Figma"],
+    },
+    {
+      name: "Password Generator",
+      description:
+      "A React-based password generator application designed to create secure and customizable passwords. The application allows users to set specific preferences such as password length and inclusion of special characters, numbers, and uppercase/lowercase letters. It provides a user-friendly interface with a responsive design, ensuring accessibility across devices. ",    
+         technologies: ["React JS" , "CSS" , "Git", "GitHub"],
+    },
+    {
+      name: "EMI Tracker",
+      description:
+      "A React-based EMI (Equated Monthly Installment) tracker application designed to help users manage and monitor their loan payments efficiently. The application provides a clear and concise breakdown of EMI schedules, total interest, and outstanding balances, empowering users to stay on top of their finances.",     
+       technologies: ["React JS","CSS", "Git", "GitHub"],
+    },
+    {
+      name: "To-do list",
+      description:
+     "A robust and interactive to-do list application built using React and Redux, designed to help users organize and prioritize tasks efficiently. This application demonstrates the seamless integration of state management with React, providing a smooth and responsive user experience.",     
+      technologies: [
+        "React JS",        
+        "CSS",
+        "Git",
+        "GitHub",
+      ],
+    },
+   
+  ],
+};
